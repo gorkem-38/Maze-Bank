e619ad60fcb49cea9515c23bfb0321ab5f126393
gorkem-38 <gorrkeem38li@gmail.com>
2022-08-25 20:43:09 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,99 @@
+// Fontawesome @import
+import '@fortawesome/fontawesome-free/js/all'
+// // Tailwind @import
+import "./assets/css/styles.css";
+// // var string = "str";
+// // var number = 31;
+// // var falsee = false;
+// // var trrue = true;
+// // var array = [];
+// // var object = {};
+// // var symbol = new Symbol();
+// // var undefined = undefined;
+// // var nulll = null;
+// // var functionn = function name(params) {
+  
+// // }
+
+// let btn = document.getElementById("switch");
+// let body = document.querySelector("body");
+
+// function darkMode() {
+//   body.classList.toggle("bg-black");
+//   body.classList.toggle("text-white");
+// }
+
+// btn.addEventListener("click", darkMode);
+
+// let azerty = 1;
+// console.log(azerty);
+
+// azerty = "azert";
+// console.log(azerty);
+
+// let test = false;
+
+// const seconds = 60;
+// console.log(seconds);
+
+// // const seconds = 80;
+
+// function test() {
+//   let namee = "gorkem";
+// };
+
+// // let namee = "gorkem";
+// // console.log(namee);
+
+// let data = {
+//   userName: "ruben",
+//   iban: "fr7651648",
+// }
+
+// // console.log(data);
+
+// // const root = document.getElementById("root");
+
+// // console.log("Vous vous appelez : " + data.userName + " " + data.iban);
+
+// // console.log(`Vous vous appelez : ${data.userName}, votre IBAN est ${data.iban}`);
+
+// // let results = `Vous vous appelez : ${data.userName}, votre IBAN est ${data.iban}`;
+
+// // let paragraph = document.createElement("p");
+// // paragraph.innerHTML = results;
+
+// // root.before(paragraph);
+
+// // class User {
+// //   constructor(username, iban, city) {
+// //     this.username = username;
+// //     this.iban = iban;
+// //     this.city = city;
+// //   };
+// // };
+
+// // console.log(new User("ruben", 76, "Meaux"));
+// // console.log(new User("gorkem", 78, "Meaux"));
+
+// // class Car {
+// //   constructor(mark, model, price, color, desc) {
+// //     this.mark = mark;
+// //     this.model = model;
+// //     this.price = price;
+// //     this.color = color;
+// //     this.desc = desc;
+// //   };
+// // };
+
+// // console.log(new Car("audi", "a6", 50000, "red", "lorem"));
+
+const namee = {
+    userName: "gorkem",
+    userAge: 20,
+    city: "Meaux",
+    iban: "fr760000",
+    color: "white",
+}
+
+console.log(namee);
