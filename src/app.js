62b5efcffe2c760a6bb53d780acb4e666570920b
gorkem-38 <gorrkeem38li@gmail.com>
2022-09-01 16:57:12 +0200

    header finish

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 // Fontawesome @import
 import '@fortawesome/fontawesome-free/js/all';
-// // Tailwind @import
+//  Tailwind @import
 import "./assets/css/styles.css";
+
+const getRandomInt = (max) => {
+  return Math.floor(Math.random() * max);
+}
+console.log(getRandomInt(3));
+
+
+let nb = 0;
+let nb1 = Math.random();
+console.log(nb1);
