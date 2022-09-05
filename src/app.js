8154a0ea48a41570399aa14d8d61b6e7797b5695
gorkem-38 <gorrkeem38li@gmail.com>
2022-09-05 14:49:15 +0200

    in progress

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ resultsBank.innerHTML = getRandom(12345678901, 99999999999);
 resultsCounter.innerHTML = getRandom(7630001007941234, 7630099999999999);
 resultsAccount.innerHTML = getRandom(10000, 99999);
 resultsRib.innerHTML = getRandom(1, 999999);
-resultsIban.innerHTML = "FR" + getRandom(10, 99);
+resultsIban.innerHTML = "FR" + getRandom(76000000000, 76999999999);
 resultsBic.innerHTML = getRandom(10, 99);
 
 const depositMoney = document.getElementById("deposit-money");
@@ -31,34 +31,3 @@ depositMoney.innerHTML = randomSold;
 
 const body = document.querySelector("body");
 
-const calculate = () => {
-  if (randomSold < 1) {
-    let results = parseInt(depositMoney.value);
-    let total = (randomSold += results);
-    resultsSold.innerHTML = total;
-    return total;
-  } else randomSold > 1;
-  {
-    body.classList.add("bg-blue-500");
-    body.classList.remove("bg-red-500");
-    let results = parseInt(inputDepot.value);
-    let total = (randomSold += results);
-    resultsSold.innerHTML = total;
-    return total;
-  };
-};
-
-btn.addEventListener("click", calculate);
-
-const calculateNegatif = () => {
-  if (randomSold > 1) {
-    let results = parseInt(inputTrade.value);
-    let total = (randomSold -= results);
-    resultsSold.innerHTML = total;
-    Math.max(-100);
-    return total;
-  } else randomSold < 1; {
-    body.classList.remove("bg-blue-500");
-    body.classList.add("bg-red-500");
-  }
-}
