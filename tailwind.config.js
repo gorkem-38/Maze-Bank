011c4ddf2c1339c1b75d3bd9f926c1d635fcdbff
gorkem-38 <gorrkeem38li@gmail.com>
2022-09-28 13:25:36 +0200

    front-end finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily: {
+        Pricedown: ["Pricedown", ...defaultTheme.fontFamily.sans],
+      },
       backgroundImage: {
         'mazebank': "url('../img/maze-bank-tower.jpg')",
       }
