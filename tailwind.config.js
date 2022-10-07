f1e1e0a4561e9e59d98dd267d0c6d9d8ff610388
gorkem-38 <gorrkeem38li@gmail.com>
2022-10-08 00:05:53 +0200

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require("tailwindcss/colors")
 
 module.exports = {
   content: ["./src/**/*.{html,js}"],
